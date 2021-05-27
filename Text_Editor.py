2188e1d9bd7dce38e79eed8eec7030d44ab5198c
decchu14 <1decchu14@gmail.com>
2021-05-27 14:43:52 +0530

    adding the text editor script

---
+++ Text_Editor.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+
+
+def open_file():
+    filepath = fd.askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+
+    if not filepath:
+        return
+
+    txt_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f'Text Editor - {filepath}')
+
+
+def save_file():
+    filepath = fd.asksaveasfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+
+    if not filepath:
+        return
+
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+
+    window.title(f'Text Editor - {filepath}')
+
+    # window
+window = tk.Tk()
+window.title("Text Editor")
+
+window.rowconfigure(0, minsize=700, weight=1)
+window.columnconfigure(1, minsize=700, weight=1)
+
+# text
+txt_edit = tk.Text(window)
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+# buttons
+fr_buttons = tk.Frame(window)
+
+btn_open = tk.Button(fr_buttons, text='Open', command=open_file)
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+
+btn_save = tk.Button(fr_buttons, text="Save As", command=save_file)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+
+# loop
+window.mainloop()
