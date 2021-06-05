98dfa4522b4567c8a432bf56774ea398d0e31c59
decchu14 <1decchu14@gmail.com>
2021-06-06 01:24:11 +0530

    adding script

--- Screen_Pet_Game.py
+++ Screen_Pet_Game.py
@@ -1,4 +1,15 @@
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, NORMAL, HIDDEN
+
+
+def show_happy(event):
+    if(event.x >= 20 and event.x <= 350) and (event.y >= 20 and event.y <= 350):
+        c.itemconfigure(cheek_left, state=NORMAL)
+        c.itemconfigure(cheek_right, state=NORMAL)
+        c.itemconfigure(mouth_happy, state=NORMAL)
+        c.itemconfigure(mouth_normal, state=HIDDEN)
+        c.itemconfigure(mouth_sad, state=HIDDEN)
+    return
+
 
 root = Tk()
 root.title("Screen_Pet_Game")
@@ -22,16 +33,26 @@ eye_left = c.create_oval(
     130, 110, 160, 170, outline='black', fill='misty rose')
 eye_right = c.create_oval(
     230, 110, 260, 170, outline='black', fill='misty rose')
-pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
-pupil_left = c.create_oval(140, 145, 150, 155, outline='black', fill='black')
+pupil_right = c.create_oval(
+    240, 145, 250, 155, outline='black', fill='black')
+pupil_left = c.create_oval(
+    140, 145, 150, 155, outline='black', fill='black')
 
-# mouth_normal = c.create_line(170, 250, 200, 272, 230, 250, smooth=1, width=2)
-# mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2)
-# mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2)
-tongue_main = c.create_rectangle(170, 250, 230, 290, outline='red', fill='red')
-tongue_tip = c.create_oval(170, 285, 230, 300, outline='red', fill='red')
-cheek_left = c.create_oval(70, 180, 120, 230, outline='pink', fill='pink')
-cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink',)
+mouth_normal = c.create_line(
+    170, 250, 200, 272, 230, 250, smooth=1, width=2, state=NORMAL)
+mouth_happy = c.create_line(170, 250, 200, 282, 230,
+                            250, smooth=1, width=2, state=HIDDEN)
+mouth_sad = c.create_line(170, 250, 200, 232, 230, 250,
+                          smooth=1, width=2, state=HIDDEN)
+tongue_main = c.create_rectangle(
+    170, 250, 230, 290, outline='red', fill='red', state=HIDDEN)
+tongue_tip = c.create_oval(
+    170, 285, 230, 300, outline='red', fill='red', state=HIDDEN)
+cheek_left = c.create_oval(
+    70, 180, 120, 230, outline='pink', fill='pink', state=HIDDEN)
+cheek_right = c.create_oval(
+    280, 180, 330, 230, outline='pink', fill='pink', state=HIDDEN)
 
 c.pack()
+c.bind('<Motion>', show_happy)
 root.mainloop()
